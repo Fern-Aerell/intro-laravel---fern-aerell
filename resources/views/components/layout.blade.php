78682e09303e48e95b4c19d3ff623f2e2f9f67c2
Fern-Aerell <fernaerell2020@gmail.com>
2024-08-22 20:39:56 +0700

    Menghapus demo.js pada layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -187,9 +187,7 @@
     <!-- overlayScrollbars -->
     <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
     <!-- AdminLTE App -->
-    <script src="dist/js/adminlte.js"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="dist/js/demo.js"></script>
+    <script src="dist/js/adminlte.js"></script
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
     <script src="dist/js/pages/dashboard.js"></script>
 </body>
